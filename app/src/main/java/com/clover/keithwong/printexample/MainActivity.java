033d02a474102e70e5d25f50c1a17af81460fe88
Keith Wong <keithryanwong@gmail.com>
2018-02-20 09:55:50 -0800

    Initial commit

---
+++ app/src/main/java/com/clover/keithwong/printexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.clover.keithwong.printexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createPrintLayout();
+        print();
+    }
+
+    protected void print() {
+
+    }
+
+    protected void createPrintLayout() {
+
+    }
+}
